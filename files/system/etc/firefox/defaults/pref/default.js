1e3ddcaa634cd1440aac6d35ac24fe701e268d4b
MoeDev <143090643+MoeDevelops@users.noreply.github.com>
2025-05-18 02:05:47 +0200

    Firefox config

---
+++ files/system/etc/firefox/defaults/pref/default.js
@@ -0,0 +1,5 @@
+pref("browser.bookmarks.file", "");
+pref("extensions.pocket.enabled", false);
+pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+pref("browser.newtabpage.pinned", "");
+pref("browser.startup.homepage", "about:home");
